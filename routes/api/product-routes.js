9b0802dabbac66119d41568faf2396022120aae0
hkidd <harrisonakidd@gmail.com>
2021-07-29 17:37:53 -0500

    Product routes are working except for put

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -23,7 +23,7 @@ router.get('/:id', async (req, res) => {
   try {
     const productData = await Product.findByPk(req.params.id, {
       // be sure to include its associated Category and Tag data
-      include: [{ model: Product }, { model: Tag }]
+      include: [{ model: Category, attributes: ['id', 'category_name'] }, { model: Tag, attributes: ['id', 'tag_name'] }]
     });
 
     if (!productData) {
@@ -106,13 +106,29 @@ router.put('/:id', (req, res) => {
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
+      console.log(err);
       res.status(400).json(err);
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
